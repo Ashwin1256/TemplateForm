719cb43ebb07e6eb3538cf57bc0df999e80b1593
Aswin singh <aswnbit@gmail.com>
2022-03-23 01:15:54 +0530

    template form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DepartmentComponent } from './department/department.component';
+import { EmployeeComponent } from './employee/employee.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: 'dept', component:DepartmentComponent},
+{path: 'employee', component:EmployeeComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingcomponents= [DepartmentComponent,EmployeeComponent]
+
+
