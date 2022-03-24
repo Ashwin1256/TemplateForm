d87538b1f1a53851bac03bfcfdbc7140e7413f81
Aswin singh <aswnbit@gmail.com>
2022-03-24 23:19:29 +0530

    complete form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,21 @@ thiscomment="this is new comment from parent";
 
      transportation:string[]=["Bus","Auto","plane","train"]
 
+     check:boolean=false;
+
+     validate(value:string){
+
+
+      if(value==='Bus'){
+       this.check= true;
+      }
+      else
+      {
+        this.check=false;
+      }
+
+     }
+
      
 onSubmit(value:User){
 console.log(value)
